7895265e8b43029046c3d75a96605df412219e4a
Gabriel Solis Vera <leo13061996@gmail.com>
2018-03-23 20:48:13 -0500

    Configuracion de prueba BD 2

--- src/main/java/com/chasquiSA/microInformacion/DAO/Conexion.java
+++ src/main/java/com/chasquiSA/microInformacion/DAO/Conexion.java
@@ -7,11 +7,12 @@ public class Conexion {
 	private static Connection conexion=null;
 	public static Connection getConexion() throws Exception{
 		//String urlDataBase = "jdbc:postgresql://localhost:5432/ChasquiSA";
-		String urlDataBase = "postgres://@ec2-54-243-185-195.compute-1.amazonaws.com:5432/dehidnnhrpsn52";
+		String urlDataBase = "jdbc:postgresql://localhost/ChasquiSA";
+		//String urlDataBase = "postgres://@ec2-54-243-185-195.compute-1.amazonaws.com:5432/dehidnnhrpsn52";
 		try {
 			Class.forName("org.postgresql.Driver");
-			//conexion = DriverManager.getConnection(urlDataBase,"postgres","1234");
-			conexion = DriverManager.getConnection(urlDataBase,"bjwhihtocljkeh","31dfb838b17298bdd936a7893ed6ab64fab55381c52677b1858ec7a12c5ac598");
+			conexion = DriverManager.getConnection(urlDataBase,"postgres","1234");
+			//conexion = DriverManager.getConnection(urlDataBase,"bjwhihtocljkeh","31dfb838b17298bdd936a7893ed6ab64fab55381c52677b1858ec7a12c5ac598");
 		}catch(Exception e) {
 			throw e;
 		}
