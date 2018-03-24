58d959485c8e19f7eb27764b4db636e72588b200
Gabriel Solis Vera <leo13061996@gmail.com>
2018-03-24 15:19:34 -0500

    PRUEBA DE SERVICIO BASICO 6 MODIFIQUE LA CONEXION

--- src/main/java/com/chasquiSA/microInformacion/DAO/Conexion.java
+++ src/main/java/com/chasquiSA/microInformacion/DAO/Conexion.java
@@ -33,4 +33,18 @@ public class Conexion {
 	public static void cerrarConexion() throws Exception{
 		conexion.close();
 	}
+	
+	/*public static Connection getConexion() throws Exception{
+		String urlDataBase = "jdbc:postgresql://localhost:5432/ChasquiSA";
+		try {
+			Class.forName("org.postgresql.Driver");
+			conexion = DriverManager.getConnection(urlDataBase,"postgres","1234");
+		}catch(Exception e) {
+			throw e;
+		}
+		return conexion;
+	}
+	public static void cerrarConexion() throws Exception{
+		conexion.close();
+	}*/
 }
