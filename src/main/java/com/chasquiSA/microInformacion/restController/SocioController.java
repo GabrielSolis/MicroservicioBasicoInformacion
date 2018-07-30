b9ad374c3e1825736e938ff46051c964f08aa07c
Gabriel Solis Vera <leo13061996@gmail.com>
2018-07-29 22:58:21 -0500

    Pruebas para generar reportes con jasper studio

--- src/main/java/com/chasquiSA/microInformacion/restController/SocioController.java
+++ src/main/java/com/chasquiSA/microInformacion/restController/SocioController.java
@@ -110,7 +110,7 @@ public class SocioController {
 	}
 	
 	
-	@GetMapping(value = "/reporte/listarSocio", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@GetMapping(value = "/reporte/listarSocios", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
 	public ResponseEntity<byte[]> generarReporteListadoSocio()throws Exception{
 		byte[] data = null;
 		SocioDAO dao = new SocioDAO();
